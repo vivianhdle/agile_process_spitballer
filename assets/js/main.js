e161883aac4689e3184590cfbbafaf12f8dce741
Vivian Le <vivianhdle@gmail.com>
2019-03-19 15:07:33 -0700

    added relevant apps class

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,9 @@ $(document).ready(initializeApp);
 function initializeApp(){
     // let newWords = new RelevantWords()
     // newWords.getSynonyms('aquarium');
+    let newApps = new RelatedApps({
+        word:'kittens',
+        displayArea:'.apps'
+    });
+    newApps.getRelatedApps();
 }
