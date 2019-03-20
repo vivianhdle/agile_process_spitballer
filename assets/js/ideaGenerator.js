c7d2b704b8727f448d4624fdc3f03e6456565599
Vivian Le <vivianhdle@gmail.com>
2019-03-20 09:58:20 -0700

    fixed app names bleeding out of container

--- assets/js/ideaGenerator.js
+++ assets/js/ideaGenerator.js
@@ -24,7 +24,7 @@ class IdeaGenerator
                 url: "https://random-word-api.herokuapp.com/word",
                 method: "get",
                 data: {
-                    key: "SL5GDWCQ",
+                    key: "0BE4B6XI",
                     number: 5
                 },
                 success: (response) =>{
