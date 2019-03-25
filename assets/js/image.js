0ba583346849bb9a1c50550dee93fbab1feef61d
Christopher-Gustavsson <chrisgustavsson1@gmail.com>
2019-03-24 21:38:51 -0700

    images rendered are randomized

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,18 +1,18 @@
 /**
  * Class representing the individual images on the page
  */
-class Image{
+class Image {
     /**
      * Creates an Image object and calls the Image API
      * @param {string} word - the word associated with the Image
      * @param callback - object holding callback functions
      */
-    constructor(word, callback){
+    constructor(word, callback) {
         this.word = word;
         this.domElement = null;
         this.callbacks = {
-            showApps:callback.showApps,
-            showRelatedWords:callback.showRelatedWords,
+            showApps: callback.showApps,
+            showRelatedWords: callback.showRelatedWords,
             deleteImageFromArray: callback.deleteImageFromArray
         }
         this.getImage();
@@ -23,8 +23,7 @@ class Image{
      * Shows the apps and words associated with the Image's word
      * Highlights the last Image that was clicked on
      */
-    handleClick = () =>
-    {
+    handleClick = () => {
         this.callbacks.showApps(this.word);
         this.callbacks.showRelatedWords(this.word);
         $(".image-wrapper > div").removeClass("selected");
@@ -38,14 +37,13 @@ class Image{
      * Creates the DOM element and its children, adds click handlers, and appends it to the DOM
      * @param {string} imageURL - the URL of the image received from the image API
      */
-    render = (imageURL) =>
-    {
+    render = (imageURL) => {
         $(".image-wrapper > div").removeClass("selected");
-        let imageContainer = $("<div>",{class: "images-container"});
-        let imageDiv = $("<div>",{class: "image"}).css("background-image", `url(${imageURL})`);
-        let wordDiv = $("<div>", {class: "word"}).text(this.word);
+        let imageContainer = $("<div>", { class: "images-container" });
+        let imageDiv = $("<div>", { class: "image" }).css("background-image", `url(${imageURL})`);
+        let wordDiv = $("<div>", { class: "word" }).text(this.word);
 
-        let deleteButton = $('<div>', {'class': 'wordCloseButton'}).text('X').click(this.deleteSelf)
+        let deleteButton = $('<div>', { 'class': 'wordCloseButton' }).text('X').click(this.deleteSelf);
         imageContainer.append(imageDiv, wordDiv, deleteButton);
 
         this.domElement = imageContainer;
@@ -56,22 +54,20 @@ class Image{
     /**
      * Calls the image API and sends the image URL to the render function
      */
-    getImage = () =>
-    {
+    getImage = () => {
         $.ajax({
             url: "https://pixabay.com/api/",
             method: "get",
             data: {
                 key: "11924912-d50c8a58952636b33dd8589d6",
                 q: this.word,
-                per_page: 3,
+                per_page: 15,
             },
             dataType: "jsonp",
-            success: (response) => 
-            {
+            success: (response) => {
                 let imageURL;
-                if (response.totalHits>0){
-                    imageURL=response.hits[0].largeImageURL;
+                if (response.totalHits > 0) {
+                    imageURL = response.hits[Math.floor(Math.random() * response.hits.length)].largeImageURL;
                 } else {
                     imageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtmmtPDV4ur79uSO4C2H9fiIgVqU340Uqxy_WF-a21Fg8V6A9Z"
                 }
@@ -83,8 +79,7 @@ class Image{
     /**
      * Deletes the DOM element and calls an ImageHolder function, removing it from the image array
      */
-    deleteSelf = () =>
-    {
+    deleteSelf = () => {
         this.domElement.remove();
         this.callbacks.deleteImageFromArray(this.word);
 
