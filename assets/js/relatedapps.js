27910d505e7f9dfdb54d67a97faa27f5463b616b
WestleyPoon <poonw@uci.edu>
2019-05-14 15:56:11 -0700

    changed number of apps retrieved to 21 reversed render order on scrollBackwards general code cleanup

--- assets/js/relatedapps.js
+++ assets/js/relatedapps.js
@@ -6,7 +6,7 @@ class RelatedApps{
      * Makes storage for dom elements and app data
      * @param {object} options - displayArea 
      */
-    constructor(options){
+    constructor(options) {
         this.displayAreas = {
             appArea: $(options.appArea),
             titleArea: $(options.titleArea),
@@ -14,34 +14,36 @@ class RelatedApps{
         };
 
         this.data = {
-            trackName:null,
-            artWork:null,
-            link:null
+            trackName: null,
+            artWork: null,
+            link: null
         };
 
         this.apps = [];
         this.marker = 3;
     }
+
     /**
      * makes an API call to search for related apps relating to a word passed in
      * @param {string} word - a string that will be pumped into the itunes app store search
      */
     getRelatedApps = word => {
         var ajaxOptions = {
-            url:"https://itunes.apple.com/search?",
-            method:"post",
-            dataType:"json",
-            data:{
+            url: "https://itunes.apple.com/search?",
+            method: "post",
+            dataType: "json",
+            data: {
                 term: word,
-                entity:'software',
-                limit: 20,
-                media:'software'
+                entity: 'software',
+                limit: 21,
+                media: 'software'
             },
-            success:this.gotRelatedApps
+            success: this.gotRelatedApps
         };
 
         $.ajax(ajaxOptions);
     }
+
     /**
      * success function, gets all the related apps and saves them in the contructor for future use
      * @param {object} response 
@@ -49,116 +51,119 @@ class RelatedApps{
     gotRelatedApps = response => {
         this.displayAreas.appArea.empty();
         this.displayAreas.titleArea.empty();
-        if (response.resultCount>0){
+
+        if (response.resultCount > 0){
             this.apps = response.results;
             $('.app-instructions').remove();
-            for (let appIndex=0;appIndex<3;appIndex++){
+
+            for (let appIndex = 0; appIndex < 3; appIndex++) {
                 this.data.trackName = this.apps[appIndex].trackName;
                 this.data.artWork = this.apps[appIndex].artworkUrl512;
                 this.data.link = this.apps[appIndex].trackViewUrl;
                 this.displayAreas.appArea.append(this.render());
             }
-            console.log(this.apps);
-            if (this.apps.length>3){
+            if (this.apps.length > 3){
                 this.appendScrollIcons();
             }
+
         } else {
             this.displayAreas.appContainer.append(
                 $('<div>',{
                     text:'NO APPS FOUND',
                     class:'app-instructions'
-                    })
-                );
+                })
+            );
         }
     }
-    scrollForward=()=>{
+
+    scrollForward = () => {
         let counter = 0;
-        if (this.marker<3){
-            this.marker=3;
-        }
-        if (this.marker<this.apps.length) {
+
+        if (this.marker < this.apps.length) {
             $('.apps').empty();
             $('.names').empty();
-            for (let appIndex=this.marker;(appIndex<this.apps.length && appIndex<this.marker+3);appIndex++){
+
+            for (let appIndex = this.marker; (appIndex < this.apps.length && appIndex < this.marker + 3); appIndex++) {
                 this.data.trackName = this.apps[appIndex].trackName;
                 this.data.artWork = this.apps[appIndex].artworkUrl512;
                 this.data.link = this.apps[appIndex].trackViewUrl;
                 this.displayAreas.appArea.append(this.render());
                 counter++;
             }
-        } else {
-            return;
+
+            this.marker = this.marker + counter;
+            this.appendScrollIcons();
         }
-        this.marker = this.marker+counter;
-        this.appendScrollIcons();
     }
-    scrollBackwards=()=>{
+
+    scrollBackwards = () => {
         let counter = 0;
-        if (this.marker>3){
+
+        if (this.marker > 3) {
             $('.apps').empty();
             $('.names').empty();
-            for (let appIndex=this.marker-3;appIndex>=0 && appIndex>this.marker-6;appIndex--){
+
+            for (let appIndex = this.marker - 6; appIndex < this.marker - 3; appIndex++) {
                 this.data.trackName = this.apps[appIndex].trackName;
                 this.data.artWork = this.apps[appIndex].artworkUrl512;
                 this.data.link = this.apps[appIndex].trackViewUrl;
                 this.displayAreas.appArea.append(this.render());
                 counter++;
             }
-        } else {
-            return;
+
+            this.marker = this.marker-counter;
+            this.appendScrollIcons();
         }
-        this.marker = this.marker-counter;
-        this.appendScrollIcons();
     }
-    renderScrollIcons(direction){
+
+    renderScrollIcons(direction) {
         let button;
-        if (direction === 'left'){
-            button = $('<button>',{
-                id:'#scroll-left'
-            })
-            const leftIcon = $('<i>',{
-                'class':'fas fa-chevron-left'
-            })
-            button.append(leftIcon);
+
+        if (direction === 'left') {
+            button = $('<button>',{id: '#scroll-left'}).append(
+                $('<i>',{'class': 'fas fa-chevron-left'})
+            );
             button.on('click', this.scrollBackwards);
+
         } else {
-            button = $('<button>',{
-                id:'#scroll-right'
-            })
-            const rightIcon = $('<i>',{
-                'class':'fas fa-chevron-right'
-            })
-            button.append(rightIcon);
+            button = $('<button>',{id: '#scroll-right'}).append(
+                $('<i>',{'class': 'fas fa-chevron-right'})
+            );
             button.on('click', this.scrollForward);
         }
-        return button
+
+        return button;
     }
-    appendScrollIcons(){
-        $('.apps').append(this.renderScrollIcons('right'));
-        $('.apps').prepend(this.renderScrollIcons('left'));
+
+    appendScrollIcons() {
+        this.displayAreas.appArea.append(this.renderScrollIcons('right'));
+        this.displayAreas.appArea.prepend(this.renderScrollIcons('left'));
     }
+
     /**
      * creates dom elements(container,title,anchortag,image) grabs data from constructor to display on the DOM
      * @returns a DOM element that has not been appended to the DOM
      */
-    render(){
-        const container = $('<div>',{
-            'class':'app'
-        })
+    render() {
         const title = $('<div>',{
             class:'app-title',
             'text':this.data.trackName
-        })
+        });
+        $('.names').append(title);
+
+        const container = $('<div>',{
+            'class':'app'
+        });
         const anchorTag = $('<a>',{
             'href':this.data.link,
             'target':'_blank'
-        })
+        });
         const image = $('<img>',{
             'src':this.data.artWork
-        })
-        $('.names').append(title);
+        });
         anchorTag.append(image);
         container.append(anchorTag);
-        return container
+
+        return container;
     }
 }
